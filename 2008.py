9db8ad767fb464b34ad01d10443e6964b25df900
HorizonFTT <HorizonFTT@outlook.com>
2020-02-19 16:36:25 +0800

    update

---
+++ 2008.py
@@ -0,0 +1,25 @@
+def is_the(word: str):
+    word = word.lower()
+    return word == 'the'
+
+
+def handle_word(word: str):
+    s = ''
+    for c in word:
+        if c.isalnum():
+            s += c
+    return s.capitalize()
+
+
+def handle_article(path_from, path_to):
+    with open(path_from, 'r') as f_in:
+        with open(path_to, 'w') as f_out:
+            for w in f_in.read().split():
+                if is_the(w):
+                    continue
+                w = handle_word(w)
+                f_out.write(w + '\n')
+
+
+if __name__ == "__main__":
+    handle_article('test.txt', 'new.txt')
