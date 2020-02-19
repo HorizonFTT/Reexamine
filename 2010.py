ef96c0f39da919dddf3c32a84d5133230ce8eb29
HorizonFTT <HorizonFTT@outlook.com>
2020-02-19 19:36:35 +0800

    update

---
+++ 2010.py
@@ -0,0 +1,53 @@
+def get_nums(path):
+    result = []
+
+    with open(path, 'rb') as f:
+        data = f.read()
+
+        for i in range(len(data)//4):
+            beg = 4 * i
+            end = beg + 4
+            b = data[beg:end]
+            num = int.from_bytes(b, 'little')
+
+            if num == 0:
+                print(f'n = {i}')
+                break
+            result.append(num)
+
+    return result
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def get_max_prime(nums):
+    for i in reversed(nums):
+        if is_prime(i):
+            return i
+
+    return None
+
+
+if __name__ == "__main__":
+    path = 'data.txt'
+    nums = get_nums(path)
+
+    print('max of nums: ', max(nums))
+    print('min of nums: ', min(nums))
+    print('max of primes: ', get_max_prime(nums))
+
+    nums.sort(reverse=True)
+    for i in range(len(nums)//3):
+        beg = 3 * i
+        end = beg + 3
+        l = nums[beg:end]
+        print(f'{i}: {max(l)} {min(l)}')
